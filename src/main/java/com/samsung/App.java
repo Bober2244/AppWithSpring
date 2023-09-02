2d9b69dc2811fa2e410998da8d9f5c7442198e91
Serg <sarutunan082@gmail.com>
2023-09-02 22:42:30 +0300

    Подключение к базе данных. Слой доступа к данным. JpaRepository

--- src/main/java/com/samsung/App.java
+++ src/main/java/com/samsung/App.java
@@ -1,10 +1,14 @@
 package com.samsung;
 
+import com.samsung.domain.Author;
+import com.samsung.repository.AuthorRepository;
+import com.samsung.service.LibDemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.h2.tools.Console;
 import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootApplication
 public class App {
@@ -13,11 +17,14 @@ public class App {
 
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 
-        try {
+        context.getBean(LibDemoService.class).bookDemo();
+
+        context.close();
+       /* try {
             Console.main(args);
         } catch (SQLException e){
             throw new RuntimeException(e);
-        }
+        } */
 
     }
 }
---
+++ src/test/java/com/samsung/repository/AuthorRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.samsung.repository;
+
+import com.samsung.domain.Author;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class AuthorRepositoryTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @DisplayName("должен добавлять автора")
+    @Test
+    void shouldInsertAuthor() {
+        Author expectedAuthor = Author.builder()
+                .id(4)
+                .name("Иван")
+                .build();
+        authorRepository.save(expectedAuthor);
+        Author actualAuthor = authorRepository.getById(4);
+
+        assertThat(actualAuthor).isEqualTo(expectedAuthor);
+    }
+}
